0c0831cd84cb0ba914ac47825622fa585bef6100
MichaelPastuch <michael.pastuch@sdxmessaging.com>
2019-11-11 14:56:03 +0000

    Removed checkbox from stamp signature

--- lib/input/signStamp.d.ts
+++ lib/input/signStamp.d.ts
@@ -2,7 +2,7 @@ import m, { ClassComponent, CVnode, CVnodeDOM } from "mithril";
 import { ISignWidget } from "../interface/widget";
 export declare function applyStamp(canvas: HTMLCanvasElement, checked: boolean): void;
 export declare class SignStamp implements ClassComponent<ISignWidget> {
-    private checked;
+    private applied;
     private canvas?;
     oncreate({ dom }: CVnodeDOM<ISignWidget>): void;
     onupdate({ attrs: { onSet } }: CVnode<ISignWidget>): void;
--- src/input/signStamp.ts
+++ src/input/signStamp.ts
@@ -8,7 +8,6 @@ import { classMap, signAspectRatio } from "../theme";
 import { pxRatio } from "../utils";
 
 export function applyStamp(canvas: HTMLCanvasElement, checked: boolean) {
-	const styles = window.getComputedStyle(canvas);
 	const ratio = pxRatio();
 	canvas.width = canvas.clientWidth * ratio;
 	canvas.height = canvas.clientHeight * ratio;
@@ -16,27 +15,24 @@ export function applyStamp(canvas: HTMLCanvasElement, checked: boolean) {
 	const context = canvas.getContext("2d") as CanvasRenderingContext2D;
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	context.textBaseline = "middle";
-	context.font = `${styles["fontWeight"]} ${fontSize}px ${styles["fontFamily"]}`;
-	// checked ? fa-check-square : fa-square
-	context.fillText(checked ? "\uf14a" : "\uf0c8", canvas.height * 0.25, canvas.height * 0.52);
 	context.font = `200 ${fontSize}px sans-serif`;
-	context.fillText(checked ? config.stampSetTxt : config.stampTxt, canvas.height, fontSize);
+	context.fillText(checked ? config.stampSetTxt : config.stampTxt, canvas.height * 0.05, fontSize);
 }
 
 export class SignStamp implements ClassComponent<ISignWidget> {
 
-	private checked: stream<boolean> = stream<boolean>(false);
+	private applied: stream<boolean> = stream<boolean>(false);
 	private canvas?: HTMLCanvasElement;
 
 	public oncreate({ dom }: CVnodeDOM<ISignWidget>) {
 		this.canvas = dom.children[0] as HTMLCanvasElement;
-		applyStamp(this.canvas, this.checked());
+		applyStamp(this.canvas, this.applied());
 	}
 
 	public onupdate({ attrs: { onSet } }: CVnode<ISignWidget>) {
 		if (this.canvas) {
-			applyStamp(this.canvas, this.checked());
-			if (this.checked()) {
+			applyStamp(this.canvas, this.applied());
+			if (this.applied()) {
 				onSet(this.canvas.toDataURL());
 			}
 		}
@@ -49,7 +45,7 @@ export class SignStamp implements ClassComponent<ISignWidget> {
 			},
 				m("canvas.aspect-ratio--object.pointer", {
 					class: classMap.icon(),
-					onclick: () => this.checked(!this.checked())
+					onclick: () => this.applied(!this.applied())
 				})
 			)
 		];
