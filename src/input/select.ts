665517753ef79d216799e6b8a5789138c97aca67
MichaelPastuch <michael.pastuch@sdxmessaging.com>
2019-11-22 15:57:31 +0000

    Bump version 1.2.19 Fix shadowed label const in select input

--- src/input/select.ts
+++ src/input/select.ts
@@ -10,7 +10,7 @@ export class SelectInput implements ClassComponent<IPropWidget> {
 
 	public view({ attrs: { field, value: val } }: CVnode<IPropWidget>) {
 		const {
-			label, id, name = id, title = label,
+			id, name = id, title = field.label,
 			required, readonly, disabled, autofocus, autocomplete,
 			containerClass, classes = "",
 			options
