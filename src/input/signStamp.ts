82a834c59a4865bfc4f31a2ee259132dcb667221
MichaelPastuch <michael.pastuch@sdxmessaging.com>
2020-04-23 15:58:10 +0100

    Trim dead code

--- src/input/signStamp.ts
+++ src/input/signStamp.ts
@@ -18,35 +18,6 @@ export function applyStamp(heightPct: number, callback: ISignWidget["onSet"]) {
 	return () => callback(createStamp(config.stampSetTxt, heightPct));
 }
 
-// export class SignStamp implements ClassComponent<ISignWidget> {
-
-// 	public oncreate({ dom }: CVnodeDOM<ISignWidget>) {
-// 		this.scaleText(dom as HTMLElement);
-// 	}
-
-// 	public onupdate({ dom }: CVnodeDOM<ISignWidget>) {
-// 		this.scaleText(dom as HTMLElement);
-// 	}
-
-// 	public view({ attrs: { heightPct, style, onSet } }: CVnode<ISignWidget>) {
-// 		return m(".aspect-ratio", { style }, [
-// 			m("span.clip", { style: { "font-family": config.signFont } }, config.stampSetTxt),
-// 			m(Button, {
-// 				label: config.stampTxt,
-// 				classes: "aspect-ratio--object",
-// 				onclick: applyStamp(heightPct, onSet)
-// 			})
-// 		]);
-// 	}
-
-// 	// Post render update text input font based on container size
-// 	private scaleText(container: HTMLElement) {
-// 		const height = container.clientHeight;
-// 		container.style.fontSize = `${0.56 * height}px`;
-// 	}
-
-// }
-
 export class SignStamp implements ClassComponent<ISignWidget> {
 
 	public view({ attrs: { heightPct, onSet } }: CVnode<ISignWidget>): Children {
