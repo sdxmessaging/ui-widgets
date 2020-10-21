6886cdb3b8563b3d88ae63cd6a933053c889fd00
Crispin Anderson <crispin.anderson@sdxmessaging.com>
2020-10-21 13:57:39 +0100

    removed commit stage

--- scriptHelpers/dist.ts
+++ scriptHelpers/dist.ts
@@ -70,7 +70,7 @@ const processArray: IProcess[] = [
 		executeMsg: 'Pulling changes on ui-widgets...'
 	},
 	{
-		execute: (version) => shell.exec(`npm run build && git add -A -- . && git commit -m 'build-file-for-version-${version}' -n -q`),
+		execute: () => shell.exec(`npm run build && git add -A -- .`),
 		rollback: () => null,
 		errMsg: 'Publish failed, could not version ui-widgets. See logs for more details',
 		executeMsg: 'Building ui-widgets...'
