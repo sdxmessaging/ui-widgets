fb9acdc26f1d6cef7deeeab61ba6fdd51dfb55e4
MichaelPastuch <michael.pastuch@sdxmessaging.com>
2020-11-09 11:16:14 +0000

    Minor tidy for file open link handling Removed duplicate cases from switch statement Added explicit types to file check functions

--- lib/input/fileSelect.d.ts
+++ lib/input/fileSelect.d.ts
@@ -1,9 +1,9 @@
 import { Children, ClassComponent, CVnode } from "mithril";
 import stream from "mithril/stream";
-import { IFileWidget } from "../interface/widget";
+import { IFile, IFileWidget } from "../interface/widget";
 export declare class FileSelect implements ClassComponent<IFileWidget> {
     protected dragging: stream<boolean>;
     view({ attrs: { field, value } }: CVnode<IFileWidget>): Children;
-    checkConfigForIcon(file: any): string;
-    returnFileExtension(file: any): any;
+    checkConfigForIcon(file?: IFile): string;
+    fileExtension(file: IFile): string;
 }
--- src/input/fileSelect.ts
+++ src/input/fileSelect.ts
@@ -2,7 +2,7 @@ import lodash from "lodash";
 import m, { Children, ClassComponent, CVnode } from "mithril";
 import stream from "mithril/stream";
 
-import { IFileWidget } from "../interface/widget";
+import { IFile, IFileWidget } from "../interface/widget";
 
 import { config } from "../config";
 import { drgCls, filCls, getIcon } from "../theme";
@@ -16,14 +16,12 @@ export class FileSelect implements ClassComponent<IFileWidget> {
 
 	public view({ attrs: { field, value } }: CVnode<IFileWidget>): Children {
 		const file = lodash.head(value());
-		return m(FileInput,
-			{
-				field,
-				multiple: false,
-				dragging: this.dragging,
-				onSet: addFiles(value, true)
-
-			},
+		return m(FileInput, {
+			field,
+			multiple: false,
+			dragging: this.dragging,
+			onSet: addFiles(value, true)
+		},
 			m(".flex.items-center.pa1.ba.b--black-20", {
 				class: this.dragging() ? drgCls() : filCls(),
 			}, [
@@ -31,31 +29,29 @@ export class FileSelect implements ClassComponent<IFileWidget> {
 					class: getIcon(config.uploadIcn)
 				}),
 				m("span.ma1.flex-auto", file ? file.name : config.addFileTxt),
-				this.checkConfigForIcon(file) ? m("i.pa1", {
+				file ? m("i.pa1", {
 					class: getIcon(this.checkConfigForIcon(file)),
-					title: 'Click to view file in new tab',
-					onclick: () => { 
-						if (file && file.name) { 
-							window.open(file.path, '_blank') 
-						}
-					}
+					title: "Click to view file in new tab",
+					onclick: file.path !== "not_set"
+						? () => window.open(file.path, "_blank")
+						: undefined
 				}) : null,
 				file ? m("i.pa1.pointer.dim", {
 					title: `Remove ${file.name}`,
 					class: getIcon(config.cancelIcn),
 					onclick: removeFile(value, file.guid)
 				}) : null,
-			]),
-		)	
+			])
+		);
 	}
 
 	// Should I move these functions to a separate config type of file ?
-	checkConfigForIcon(file: any) {
+	checkConfigForIcon(file?: IFile) {
 		let icon = '';
-		if (file) { 
-			const extension = this.returnFileExtension(file).toLowerCase();
+		if (file) {
+			const extension = this.fileExtension(file).toLowerCase();
 			switch (extension) {
-				case 'doc': 
+				case 'doc':
 				case 'docx':
 				case 'dot':
 				case 'wbk':
@@ -64,102 +60,95 @@ export class FileSelect implements ClassComponent<IFileWidget> {
 				case 'dotm':
 				case 'docb':
 				case 'txt':
-				{
-					icon = config.wordDocIcn
-					break;
-				};
-				case 'webm': 
-				case 'mkv': 
-				case 'flv': 
-				case 'vob': 
-				case 'ogv': 
-				case 'ogg': 
-				case 'drc': 
-				case 'gifv': 
-				case 'mng': 
-				case 'avi': 
-				case 'mts': 
-				case 'm2ts': 
-				case 'mov': 
-				case 'qt': 
-				case 'wmv': 
-				case 'yuv': 
-				case 'rm': 
-				case 'rmvb': 
-				case 'viv': 
-				case 'asf': 
-				case 'amv': 
-				case 'mp4': 
-				case 'm4p': 
-				case 'm4v': 
-				case 'mpg': 
-				case 'mp2': 
-				case 'mpeg': 
-				case 'mpe': 
-				case 'mpv': 
-				case 'm2v': 
-				case 'm4v': 
-				case 'svi': 
-				case '3gp': 
-				case 'mxf': 
-				case 'roq': 
-				case 'nsv': 
-				case 'flv': 
-				case 'f4v': 
-				case 'f4p': 
-				case 'f4a': 
-				case 'f4b': 
-				{
-					icon = config.videoFileIcn;
-					break;
-				}
-
+					{
+						icon = config.wordDocIcn;
+						break;
+					}
+				case 'webm':
+				case 'mkv':
+				case 'flv':
+				case 'vob':
+				case 'ogv':
+				case 'drc':
+				case 'gifv':
+				case 'mng':
+				case 'avi':
+				case 'mts':
+				case 'm2ts':
+				case 'mov':
+				case 'qt':
+				case 'wmv':
+				case 'yuv':
+				case 'rm':
+				case 'rmvb':
+				case 'viv':
+				case 'asf':
+				case 'amv':
+				case 'mp4':
+				case 'm4p':
+				case 'm4v':
+				case 'mpg':
+				case 'mp2':
+				case 'mpeg':
+				case 'mpe':
+				case 'mpv':
+				case 'm2v':
+				case 'svi':
+				case '3gp':
+				case 'mxf':
+				case 'roq':
+				case 'nsv':
+				case 'f4v':
+				case 'f4p':
+				case 'f4a':
+				case 'f4b':
+					{
+						icon = config.videoFileIcn;
+						break;
+					}
 				case 'pdf': {
 					icon = config.pdfFileIcn;
 					break;
-
 				}
+				case 'pcm':
+				case 'wav':
+				case 'aiff':
+				case 'mp3':
+				case 'aac':
+				case 'ogg':
+				case 'wma':
+				case 'flac':
+				case 'alac':
+					{
+						icon = config.musicFileIcn;
+						break;
+					}
 
-				case 'pcm': 
-				case 'wav': 
-				case 'aiff': 
-				case 'mp3': 
-				case 'aac': 
-				case 'ogg': 
-				case 'wma': 
-				case 'flac': 
-				case 'alac': 
-				{
-					icon = config.musicFileIcn;
-					break;
-				}
-
-				case 'xls': 
-				case 'xlt': 
-				case 'xlm': 
-				case 'xlsx': 
-				case 'xlsm': 
-				case 'xltx': 
-				case 'xltm': 
-				case 'xlsb': 
-				case 'xla': 
-				case 'xlam': 
-				case 'xll': 
-				case 'xlw': 
-				{
-					icon = config.excelFileIcn;
-					break;
-				}
-
+				case 'xls':
+				case 'xlt':
+				case 'xlm':
+				case 'xlsx':
+				case 'xlsm':
+				case 'xltx':
+				case 'xltm':
+				case 'xlsb':
+				case 'xla':
+				case 'xlam':
+				case 'xll':
+				case 'xlw':
+					{
+						icon = config.excelFileIcn;
+						break;
+					}
 				case 'html':
 				case 'js':
-				case 'css': 
+				case 'css':
 				case 'scss':
 				case 'java':
-				{
-					icon = config.codeFileIcn;
-					break;
-				}
+					{
+						icon = config.codeFileIcn;
+						break;
+					}
 				case 'jpg':
 				case 'jpeg':
 				case 'png':
@@ -168,22 +157,21 @@ export class FileSelect implements ClassComponent<IFileWidget> {
 				case 'svg':
 				case 'webp': {
 					icon = config.imageIcn;
-					break; 	
+					break;
 				}
-	
-				default : {
+				default: {
 					icon = config.fileIcn;
 					break;
 				}
 			}
-		} 
+		}
 		return icon;
 	}
 
-	returnFileExtension(file: any) {
+	fileExtension(file: IFile) {
 		const fileItem = file.name.split(".");
 		const fileIndex = fileItem.length - 1;
 		const fileExtension = fileItem[fileIndex];
-		return fileExtension
-	}	
+		return fileExtension;
+	}
 }
