e66fb306a628e8af3b117ca2efc88eeeef54cdc9
MichaelPastuch <michael.pastuch@sdxmessaging.com>
2020-12-14 23:27:38 +0000

    Converted omniFileInput spacing to tabs

--- src/input/omniFileInput.ts
+++ src/input/omniFileInput.ts
@@ -1,56 +1,58 @@
 import lodash from "lodash";
 import m, { Children, ClassComponent, CVnode } from "mithril";
-import { FileInput } from "./fileInput";
 import stream from "mithril/stream";
+
 import { IFile, IFileWidget } from "../interface/widget";
+
+import { config } from "../config";
 import { drgCls, filCls, imgMaxSize, getIcon } from "../theme";
 import { isImage, dataURItoBlob, fileConstructor, guid, imgSrc, resizeImage, getFileTypeIcon } from "../utils";
+
+import { FileInput } from "./fileInput";
 import { removeFile } from "./fileMulti";
-import { config } from "../config";
 
 export function addOmniFile(fileList: stream<IFile[]>, replace = true) {
 	return (addList: FileList | null) => {
-        const newFileList = replace ? [] : fileList();
-        return new Promise((resolve, reject) => {
-            if (addList && addList.length) {
-                const addFile = addList[0];
-                if (isImage(addFile.type)) {
-                    resizeImage(addFile, config.imageMaxSize, addFile.type).then((dataURL) => {
-                        const newFile = fileConstructor(dataURItoBlob(dataURL), addFile.name);
-                        newFileList.push({
-                            guid: guid(),
-                            name: newFile.name,
-                            path: "not_set",
-                            file: newFile,
-                            dataUrl: dataURL
-                        });
-                        resolve();
-                    });
-                } else {
-                    newFileList.push({
-                        guid: guid(),
-                        name: addFile.name,
-                        path: "not_set",
-                        file: addFile,
-                    });
-                    resolve();
-                }
-            } else {
-                reject("No file has been input");
-            }
-        }).then(() => {
-            fileList(newFileList);
-            m.redraw();
-        })
-    };
-    
+		const newFileList = replace ? [] : fileList();
+		return new Promise<void>((resolve, reject) => {
+			if (addList && addList.length) {
+				const addFile = addList[0];
+				if (isImage(addFile.type)) {
+					resizeImage(addFile, config.imageMaxSize, addFile.type).then((dataURL) => {
+						const newFile = fileConstructor(dataURItoBlob(dataURL), addFile.name);
+						newFileList.push({
+							guid: guid(),
+							name: newFile.name,
+							path: "not_set",
+							file: newFile,
+							dataUrl: dataURL
+						});
+						resolve();
+					});
+				} else {
+					newFileList.push({
+						guid: guid(),
+						name: addFile.name,
+						path: "not_set",
+						file: addFile,
+					});
+					resolve();
+				}
+			} else {
+				reject("No file has been input");
+			}
+		}).then(() => {
+			fileList(newFileList);
+			m.redraw();
+		});
+	};
 }
 
 export class OmniFileInput implements ClassComponent<IFileWidget> {
-    
+
 	protected dragging: stream<boolean> = stream<boolean>(false);
 
-    public view({ attrs: { field, value } }: CVnode<IFileWidget>): Children {
+	public view({ attrs: { field, value } }: CVnode<IFileWidget>): Children {
 		const file = lodash.head(value());
 		const { containerClass = "" } = field;
 		return m("fieldset.pa0.bn", {
@@ -59,28 +61,28 @@ export class OmniFileInput implements ClassComponent<IFileWidget> {
 			m(FileInput, {
 				field,
 				defaultAccept: "*",
-				multiple: false, 
+				multiple: false,
 				dragging: this.dragging,
 				onSet: addOmniFile(value, true)
 			},
 				m(".flex.items-center.pa1.ba.b--black-20.dt.relative", {
 					class: this.dragging() ? drgCls() : filCls()
 				}, file?.dataUrl ? [
-                    m('.w-100.tc', {},
-                        m("img.img.contain", {
-                            title: file.name,
-                            src: imgSrc(file.path, file.dataUrl),
-                            style: imgMaxSize()
-                        }),
-                        m(".absolute.top-0.right-0.pa1.pointer.dim", {
-                            title: `Remove ${file.name}`,
-                            onclick: removeFile(value, file.guid)
-                        }, m("i.pa1", {
-                            class: getIcon(config.cancelIcn)
-                        }))
-                    )
+					m(".w-100.tc", {},
+						m("img.img.contain", {
+							title: file.name,
+							src: imgSrc(file.path, file.dataUrl),
+							style: imgMaxSize()
+						}),
+						m(".absolute.top-0.right-0.pa1.pointer.dim", {
+							title: `Remove ${file.name}`,
+							onclick: removeFile(value, file.guid)
+						}, m("i.pa1", {
+							class: getIcon(config.cancelIcn)
+						}))
+					)
 				] : !file?.dataUrl ? [
-                    m("i.pa1", {
+					m("i.pa1", {
 						class: getIcon(config.uploadIcn)
 					}),
 					m("span.ma1.flex-auto", file ? file.name : config.addFileTxt),
@@ -96,8 +98,8 @@ export class OmniFileInput implements ClassComponent<IFileWidget> {
 						class: getIcon(config.cancelIcn),
 						onclick: removeFile(value, file.guid)
 					}) : null,
-                ] : m("i.fa-2x.dtc.v-mid", {
-                    class: getIcon(config.uploadIcn)
+				] : m("i.fa-2x.dtc.v-mid", {
+					class: getIcon(config.uploadIcn)
 				}))
 			)
 		]);
