765b5d4fe7193a660b31e1170bcf83053ffeed59
sdx-crispin <crispin.anderson@sdxmessaging.com>
2020-12-22 16:38:56 +0000

    build

--- lib/theme.d.ts
+++ lib/theme.d.ts
@@ -1,4 +1,5 @@
 import stream from "mithril/stream";
+import { IClassMap } from "./interface/theme";
 import { ITheme, TThemeUpdate } from "./interface/theme";
 export declare function imgMaxSize(): {
     "max-height": string;
@@ -12,6 +13,10 @@ export declare const styleSm: {
 export declare const styleLg: {
     "max-width": string;
 };
+export declare const theme: Readonly<IClassMap>;
+export declare function updateClasses(newConfig: Partial<IClassMap>): void;
+export declare function updateButtonContext(newButtonContext: Record<string, string>): void;
+export declare function getButtonContext(key?: string): string;
 export declare const classMap: ITheme;
 export declare const dspLblCls: stream<string>;
 export declare const lblCls: stream<string>;
@@ -25,5 +30,3 @@ export declare const btnClass: stream<string>;
 export declare const navClass: stream<string>;
 export declare function updateTheme(newTheme: Partial<TThemeUpdate>): void;
 export declare function getIcon(iconClass: string): string;
-export declare function updateButtonContext(newButtonContext: Record<string, string>): void;
-export declare function getButtonContext(key?: string): string;
