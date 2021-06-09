1703fe4bc17944c81aa4af33468cf5af9101910e
MichaelPastuch <michael.pastuch@sdxmessaging.com>
2021-06-09 11:37:10 +0100

    Updated dist script

--- scriptHelpers/dist.ts
+++ scriptHelpers/dist.ts
@@ -1,32 +1,30 @@
-
 const shell = require('shelljs');
 const currentVersion = require('../package.json').version;
+const path = require('path');
 
-enum versionBump {
-	MAJOR = 'major',
-	MINOR = 'minor',
-	PATCH = 'patch'
+const enum versionBump {
+	MAJOR = "major",
+	MINOR = "minor",
+	PATCH = "patch"
 }
 
 const bumpType = process.argv.reduce(
 	(acc, arg): versionBump =>
-		arg.includes('BUMP') ? arg.split('=')[1] as versionBump : acc as versionBump,
+		arg.includes("BUMP") ? arg.split("=")[1].toLowerCase() as versionBump : acc as versionBump,
 	versionBump.PATCH
-) as versionBump || versionBump.PATCH;
+) as versionBump;
 
-const bumpVersion = ((bumpType?: versionBump) => {
-	const [major, minor, patch] = currentVersion.split('.').map((str: string) => Number(str));
-
+const bumpVersion = ((bumpType: versionBump) => {
+	const [major, minor, patch] = currentVersion.split(".").map((str: string) => Number(str));
 	switch (bumpType) {
 		case versionBump.MAJOR:
-			return `${major + 1}.${minor}.${patch}`
+			return `${major + 1}.0.0`
 		case versionBump.MINOR:
-			return `${major}.${minor + 1}.${patch}`
+			return `${major}.${minor + 1}.0`
 		default:
 			return `${major}.${minor}.${patch + 1}`
 	}
-
-})(bumpType)
+})(bumpType);
 
 interface IProcess {
 	execute: (version: string) => { code: number },
@@ -45,50 +43,65 @@ class ProcessStack {
 		this.processArray = processArray;
 	}
 
-	execute = async () => {
+	public async execute() {
 		for (let i = 0; i < this.processArray.length; i++) {
 			let process = this.processArray[i];
 			console.log(process.executeMsg);
-			let result = await process.execute(this.version);
-			if (result.code !== 0) {
+			try {
+				let result = await process.execute(this.version);
+				if (result && result.code !== 0) {
+					console.warn(process.errMsg);
+					return process.rollback(this.version);
+				}
+			}
+			catch (err) {
+				console.log(err)
 				console.warn(process.errMsg);
 				return process.rollback(this.version);
 			}
 			if (i === this.processArray.length - 1) {
-				console.log('\x1b[32m', `Hoorah! You published a new ${bumpVersion} version - `, this.version);
+				console.log('\x1b[32m', `Hoorah!! You published a new ${bumpType} version - ${this.version}`);
 			}
 		}
 	}
 }
 
-
 const processArray: IProcess[] = [
+	// Pull
 	{
-		execute: () => shell.exec(`git pull -q`),
+		execute: () => shell.exec("git pull -q"),
 		rollback: () => null,
-		errMsg: 'Publish failed, could not pull changes ui-widgets. See logs for more details',
-		executeMsg: 'Pulling changes on ui-widgets...'
+		errMsg: "Publish failed, could not pull changes ui-widgets. See logs for more details",
+		executeMsg: "Pulling changes on ui-widgets..."
 	},
+	// Test
 	{
-		execute: () => shell.exec(`npm run build && git add -A -- .`),
+		execute: () => shell.exec("npm run test"),
 		rollback: () => null,
-		errMsg: 'Publish failed, could not version ui-widgets. See logs for more details',
-		executeMsg: 'Building ui-widgets...'
+		errMsg: "Publish failed, could not pull changes ui-widgets. See logs for more details",
+		executeMsg: "Pulling changes on ui-widgets..."
 	},
+	// Build, add and commit
 	{
-		execute: (version) => shell.exec(`npm version ${version}`),
-		rollback: () => null,
-		errMsg: 'Publish failed, could not version ui-widgets. See logs for more details',
-		executeMsg: 'Running npm version on ui-widgets...'
+		execute: () => shell.exec(`npm run build && git add -A -- . && git commit -m "full build"`),
+		rollback: () => shell.exec("git reset --hard HEAD~1"),
+		errMsg: "Publish failed, could not commit ui-widgets. See logs for more details",
+		executeMsg: "Building ui-widgets..."
 	},
+	// Version (+git tag)
 	{
-		execute: () => shell.exec(`git push --follow-tags`),
-		rollback: (version) => {
-			shell.exec(`git tag -d v${version}`);
-		},
-		errMsg: 'Publish failed. See logs for more details',
-		executeMsg: 'Git push on ui-widgets...'
+		execute: (version: string) => shell.exec(`npm version ${version}`),
+		rollback: (version: string) => shell.exec(`git tag -d v${version} && git reset --hard HEAD~2`),
+		errMsg: "Publish failed, could not version ui-widgets. See logs for more details",
+		executeMsg: "Running npm version on ui-widgets..."
 	},
+	// Push follow tags
+	{
+		execute: () => shell.exec("git push --follow-tags --no-verify"),
+		rollback: (version: string) => shell.exec(`git tag -d v${version} && git reset --hard HEAD~2`),
+		errMsg: "Publish failed, See logs for more details",
+		executeMsg: "Git push..."
+	}
 ];
 
 const processStack = new ProcessStack(processArray, bumpVersion);
