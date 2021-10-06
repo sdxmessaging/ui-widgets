85e9433fa473cb1f59a0a48d50a3fea0a1e9a715
nathan-yung-sdx <nathan.yung@sdxmessaging.com>
2021-10-06 15:05:51 +0100

    added text area to return either internal/external label

---
+++ src/input/textareaInputExternalLabel.ts
@@ -0,0 +1,36 @@
+import m, { ClassComponent, CVnode } from "mithril";
+
+import { IPropWidget } from "../interface/widget";
+
+import { inputWrapperCls, textareaCls, wrapperCls } from "../theme";
+import { getLabel, setValue } from "../utils";
+import { propInvalid } from "../validation";
+
+export class TextareaInputExternalLabel implements ClassComponent<IPropWidget> {
+
+	public view({ attrs: { field, value } }: CVnode<IPropWidget>) {
+		const {
+			label, id, name = id, title = label, placeholder,
+			required, readonly, disabled, autofocus, autocomplete, spellcheck,
+			instant, uiClass = {},
+		} = field;
+		return m("fieldset", {
+			class: wrapperCls(uiClass, disabled)
+		}, [
+			getLabel(id, uiClass, label, required),
+			m("div", {
+				class: inputWrapperCls(uiClass, propInvalid(field, value()))
+			}, m("textarea.w-100.bg-transparent.bn.outline-0.h-100", {
+				id, name, title,
+				placeholder, required, readonly, disabled, autofocus, autocomplete, spellcheck,
+				class: textareaCls(uiClass),
+				value: value(),
+				style: { resize: "none" },
+				// Update value on change or input ("instant" option)
+				[instant ? "oninput" : "onchange"]: setValue(value)
+			})
+			)
+		]);
+	}
+
+}
