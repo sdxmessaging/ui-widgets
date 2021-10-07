e6c2ff7efe869f2ba6986e56ad07c040d2b9a6b4
nathan-yung-sdx <nathan.yung@sdxmessaging.com>
2021-10-07 10:20:02 +0100

    changing height/font-size support

--- src/input/inputInternalLabel.ts
+++ src/input/inputInternalLabel.ts
@@ -1,11 +1,11 @@
-import m, { ClassComponent, CVnode } from "mithril";
+import m, { ClassComponent, CVnode, CVnodeDOM } from "mithril";
 import { FieldType, IPropWidget } from "../interface/widget";
 import { inputWrapperCls, wrapperCls } from "../theme";
 import { propInvalid } from "../validation";
 
 export class InputInternalLabel implements ClassComponent<IPropWidget> {
 	protected selected = false;
-
+	private wrapperHeight!: number;
 	protected focusIn = () => {
 		this.selected = true;
 	};
@@ -18,25 +18,31 @@ export class InputInternalLabel implements ClassComponent<IPropWidget> {
 		return m('span', vnode.attrs.value());
 	}
 
+	public oncreate({ dom }: CVnodeDOM<IPropWidget>) {
+		this.wrapperHeight = dom.clientHeight;
+		m.redraw();
+	}
+
 	public view(vnode: CVnode<IPropWidget>) {
 		const { attrs: { field, value, xform = value } } = vnode;
 		const {
 			label, type = FieldType.text, disabled, uiClass = {}, shrink
 		} = field;
 		const floatLabel = shrink || value() || this.selected;
+
 		return m("fieldset.relative.flex.mt2", {
-			class: type === FieldType.hidden ? "clip" : wrapperCls(uiClass, disabled),
+			class: type === FieldType.hidden ? "clip" : wrapperCls(uiClass, disabled)
 		}, [
 			m("label.db.top-0.left-0.absolute.z-999", {
 				title: label,
 				style: {
-					transform: floatLabel ? 'translate(15px, -7px) scale(0.7)' : 'translate(10px, 9px) scale(1)',
+					transform: floatLabel ? 'translate(10px, -0.4em) scale(0.7)' : `translate(10px, calc(${this.wrapperHeight / 2}px - 0.5em)) scale(1)`,
 					transition: `transform ${floatLabel ? '0.3s' : '0.4s'} ease-in-out, opacity 0.4s ease-in-out`,
+					display: this.wrapperHeight ? "inherit" : "none",
 					opacity: floatLabel ? 0.8 : 0.6,
 					transformOrigin: 'top left',
 					// Essential for the legend to fit the correct amount of space
 					wordSpacing: '2px',
-					fontSize: '1rem',
 				}
 			}, label),
 			m(".flex.w-100.pa2", {
@@ -49,14 +55,14 @@ export class InputInternalLabel implements ClassComponent<IPropWidget> {
 						pointerEvents: 'auto',
 					}
 				}, this.viewInput(vnode)),
-				m('fieldset.absolute.ba.b--light-gray',
+				m('fieldset.absolute.ba.b--light-gray.ph1',
 					{
 						style: {
 							top: '-5px',
 							right: '-2px',
 							bottom: '0px',
 							left: '-2px',
-							padding: '0 8px',
+							border: 'solid 1px'
 						},
 						class: inputWrapperCls(uiClass, propInvalid(field, xform())),
 
@@ -66,7 +72,7 @@ export class InputInternalLabel implements ClassComponent<IPropWidget> {
 							visibility: 'hidden',
 							maxWidth: floatLabel ? '100%' : '0.01px',
 							height: '11px',
-							fontSize: '0.7rem',
+							fontSize: '0.7em',
 						}
 					}, m('span', {
 						style: {
