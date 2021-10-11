03f95702a34f0318dcfafd455219ba6bf0569cfc
sdx-joe <joe.summerhayes@sdxmessaging.com>
2021-10-11 10:47:10 +0100

    update password input to use new input scaffolding

--- src/input/password.ts
+++ src/input/password.ts
@@ -1,32 +1,28 @@
 import m, { ClassComponent, CVnode } from "mithril";
 import stream from "mithril/stream";
-
-import { IPropWidget } from "../interface/widget";
-
+import { IPropWidget, LabelType } from "../interface/widget";
 import { config } from "../config";
-import { inputCls, inputWrapperCls, wrapperCls } from "../theme";
-import { getLabel, setValue } from "../utils";
-import { propInvalid } from "../validation";
+import { inputCls } from "../theme";
+import { setValue } from "../utils";
+import { Basic } from "./layout/basic";
+import { FloatLabel } from "./layout/floatLabel";
 
 export class PasswordInput implements ClassComponent<IPropWidget> {
 
 	private showPassword = stream<boolean>(false);
 
-	public view({ attrs: { field, value } }: CVnode<IPropWidget>) {
+	public view({ attrs }: CVnode<IPropWidget>) {
+		const { field, value } = attrs;
 		const {
 			label, id, name = id, title = label, placeholder,
 			maxlength, minlength, required,
 			readonly, disabled, autofocus, autocomplete,
 			pattern, inputmode,
-			instant, uiClass = {},
+			instant, uiClass = {}, layout
 		} = field;
-		return m("fieldset", {
-			class: wrapperCls(uiClass, disabled)
-		}, [
-			getLabel(id, uiClass, label, required),
-			m("div.w-100.flex.items-center", {
-				class: inputWrapperCls(uiClass, propInvalid(field, value()))
-			},
+
+		return m(layout === LabelType.default ? Basic : FloatLabel, attrs,
+			m('.flex.flex-row', [
 				m("input.w-100.bg-transparent.bn.outline-0", {
 					id, name, title, placeholder,
 					type: this.showPassword() ? "text" : "password",
@@ -45,8 +41,8 @@ export class PasswordInput implements ClassComponent<IPropWidget> {
 					class: this.showPassword() ? config.hidePassIcn : config.showPassIcn,
 					onclick: () => this.showPassword(!this.showPassword())
 				})
+			]
 			)
-		]);
+		);
 	}
-
 }
