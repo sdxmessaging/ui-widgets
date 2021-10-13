ad65cf8dc8e947e7b877d214edb4ceacb37b6af1
nathan-yung-sdx <nathan.yung@sdxmessaging.com>
2021-10-13 11:17:46 +0100

    cleaned up dateInput

--- src/input/dateInput.ts
+++ src/input/dateInput.ts
@@ -2,7 +2,7 @@ import lodash from "lodash";
 import m, { ClassComponent, CVnode, CVnodeDOM } from "mithril";
 import stream from "mithril/stream";
 
-import { FieldType, IOptionField, IPropWidget, TProp, TPropStream } from "../interface/widget";
+import { FieldType, IOptionField, IPropWidget, TField, TProp, TPropStream } from "../interface/widget";
 
 import { DateWidth, inputCls } from "../theme";
 import { handleDateChange, setCustomValidityMessage, TDateInputType } from "../utils";
@@ -10,7 +10,7 @@ import { handleDateChange, setCustomValidityMessage, TDateInputType } from "../u
 import { layoutFixed } from "./layout/layoutFixedLabel";
 
 type TDateType = 'day' | 'month' | 'year';
-type TDateValueType = TDateType | "literal";
+
 function dateInputIds(type: TDateType) {
 	switch (type) {
 		case 'day': return 'dd';
@@ -30,7 +30,6 @@ export class DateInput implements ClassComponent<IPropWidget> {
 	private buildDate() {
 		this.date(`${this.year()}-${this.month()}-${this.day()}`);
 	}
-
 
 	private updateInputs(valueStream: TPropStream) {
 		const newYear = parseInt(this.year());
@@ -54,8 +53,7 @@ export class DateInput implements ClassComponent<IPropWidget> {
 
 	private dom!: Element;
 	private dateParts!: Intl.DateTimeFormatPart[];
-	private locale = stream<string | undefined>();
-
+	private locale = stream<string | undefined>(undefined);
 
 	private findNextInput(type: TDateType) {
 		const index = this.dateInputAdvanceOrder.indexOf(type);
@@ -73,6 +71,14 @@ export class DateInput implements ClassComponent<IPropWidget> {
 		this.dateInputAdvanceOrder = lodash.filter(this.dateInputOrder, (type) => {
 			return type !== "literal";
 		});
+	}
+
+	private setLocale(field: TField) {
+		const { options } = field as IOptionField;
+		const locale = options && options.length ? options[0].value as string : undefined;
+		if (locale !== this.locale()) {
+			this.locale(locale);
+		}
 	}
 
 	public oninit({ attrs }: CVnode<IPropWidget>) {
@@ -96,13 +102,10 @@ export class DateInput implements ClassComponent<IPropWidget> {
 			}
 		});
 
-		const { options } = attrs.field as IOptionField;
-		const locale = options && options.length ? options[0].value as string : undefined;
-		this.setDateInputs(locale);
-
 		this.locale.map((newVal) => {
 			this.setDateInputs(newVal);
 		});
+		this.setLocale(attrs.field);
 	}
 
 	public oncreate({ dom }: CVnodeDOM<IPropWidget>) {
@@ -118,13 +121,8 @@ export class DateInput implements ClassComponent<IPropWidget> {
 		this.day.end(true);
 	}
 
-	public onbeforeupdate({ attrs }: CVnode<IPropWidget>) {
-		const { options } = attrs.field as IOptionField;
-		const locale = options && options.length ? options[0].value as string : undefined;
-
-		if (locale !== this.locale()) {
-			this.locale(locale);
-		}
+	public onbeforeupdate({ attrs: { field } }: CVnode<IPropWidget>) {
+		this.setLocale(field);
 	}
 
 	public view({ attrs }: CVnode<IPropWidget>) {
@@ -133,12 +131,14 @@ export class DateInput implements ClassComponent<IPropWidget> {
 			required, readonly, disabled,
 			uiClass = {},
 		} = attrs.field as IOptionField;
-
 		const classStr = inputCls(uiClass);
 
-		const dateInputSet: Record<TDateValueType, m.Vnode> = {
-			day: m(".dib", [
-				m("input.w-100.bg-transparent.bn.outline-0", {
+		return m(layoutFixed, attrs, this.dateParts.map(({ type, value }) => {
+			if (type === 'literal') {
+				return m('span.self-center', value);
+			}
+			else if (type === "day") {
+				return m(".dib", m("input.w-100.bg-transparent.bn.outline-0", {
 					id: `${id}-dd`, name: `${name}-dd`,
 					type: FieldType.text, placeholder: "DD",
 					minlength: "2", maxlength: "2",
@@ -146,8 +146,7 @@ export class DateInput implements ClassComponent<IPropWidget> {
 					required, readonly, disabled,
 					value: this.day(),
 					oninput: () => {
-						handleDateChange(this.day, id, "dd", this.dom,
-							this.findNextInput('day'));
+						handleDateChange(this.day, id, "dd", this.dom, this.findNextInput('day'));
 						this.updateInputs(attrs.value);
 					},
 					class: classStr,
@@ -155,11 +154,10 @@ export class DateInput implements ClassComponent<IPropWidget> {
 						maxWidth: DateWidth.dd,
 						textAlign: this.day() && this.day().length === 2 ? "center" : "left"
 					}
-				})
-			]),
-			literal: m('span.self-center', '/'),
-			month: m(".dib", [
-				m("input.w-100.bg-transparent.bn.outline-0", {
+				}));
+			}
+			else if (type === 'month') {
+				return m(".dib", m("input.w-100.bg-transparent.bn.outline-0", {
 					id: `${id}-mm`, name: `${name}-mm`,
 					type: FieldType.text, placeholder: "MM",
 					minlength: "2", maxlength: "2",
@@ -175,10 +173,10 @@ export class DateInput implements ClassComponent<IPropWidget> {
 						maxWidth: DateWidth.mm,
 						textAlign: this.month() && this.month().length === 2 ? "center" : "left"
 					}
-				})
-			]),
-			year: m(".dib", [
-				m("input.w-100.bg-transparent.bn.outline-0", {
+				}));
+			}
+			else if (type === 'year') {
+				return m(".dib", m("input.w-100.bg-transparent.bn.outline-0", {
 					id: `${id}-yyyy`, name: `${name}-yyyy`,
 					type: FieldType.text, placeholder: "YYYY",
 					minlength: "4", maxlength: "4",
@@ -188,19 +186,14 @@ export class DateInput implements ClassComponent<IPropWidget> {
 					oninput: () => {
 						handleDateChange(this.year, id, "yyyy", this.dom, this.findNextInput('year'));
 						this.updateInputs(attrs.value);
-
 					},
 					class: classStr,
 					style: {
 						maxWidth: DateWidth.yyyy
 					}
-				})
-			])
-		};
-
-		return m(layoutFixed, attrs, lodash.map(this.dateInputOrder, (type: Intl.DateTimeFormatPartTypes) => {
-			return dateInputSet[type as TDateValueType];
+				}));
+			}
+			return null;
 		}));
 	}
-
 }
