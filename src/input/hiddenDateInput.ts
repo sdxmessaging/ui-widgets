ef5ac2e5fcb1051be33057e7b336e67d8a109c9f
nathan-yung-sdx <nathan.yung@sdxmessaging.com>
2021-10-14 17:37:57 +0100

    removed imports

--- src/input/hiddenDateInput.ts
+++ src/input/hiddenDateInput.ts
@@ -1,10 +1,5 @@
-// import lodash from "lodash";
 import m, { ClassComponent, CVnode } from "mithril";
-
 import { IPropWidget } from "../interface/widget";
-
-// import { focusLastInput } from "../utils";
-
 export class HiddenDateInput implements ClassComponent<IPropWidget> {
 	public view({ attrs }: CVnode<IPropWidget>) {
 		const { id } = attrs.field;
