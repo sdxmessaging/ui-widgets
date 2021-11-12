cfaced2850a05af6ec6fe6e7f7d2410ec354fe10
Ted While <ted.while@sdxmessaging.com>
2021-11-12 10:25:00 +0000

    basic testing of validationBase

--- src/validationBase.spec.ts
+++ src/validationBase.spec.ts
@@ -1,5 +1,33 @@
+import { Children, CVnodeDOM } from "mithril";
+import { IPropWidget } from "./interface/widget";
+import { ValidationBase } from "./validationBase";
+import { Label } from "./input/label"
+import stream from "mithril/stream";
 
 // Talk to michael about testing this.
-test("placeholder", () => {
-    expect(true).toBe(true);
+class ValidationTest extends ValidationBase {
+
+    public get getInvalid(): boolean {
+        return this.invalid;
+    }
+
+    view(): Children {
+        throw new Error("Method not implemented.");
+    }
+
+}
+
+
+test("onupdate-propInvalid", () => {
+    const vbase = new ValidationTest();
+    const root : Element = window.document.createElement("label");
+    const attrs = {value: stream<string>("hello"), field: {id: "test", maxlength: 2}};
+    const label = new Label();
+    const cv : CVnodeDOM<IPropWidget> = {
+        dom: root, attrs: attrs,
+        tag: "", state: label
+    };
+    expect(vbase.getInvalid).toBe(false);
+    vbase.onupdate(cv);
+    expect(vbase.getInvalid).toBe(true);
 });
