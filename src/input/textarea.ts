700f5fb6bd9d84902c415f850e5946c62be95e60
sdx-joe <joe.summerhayes@sdxmessaging.com>
2022-08-22 15:56:04 +0100

    add min and mx length to textarea

--- src/input/textarea.ts
+++ src/input/textarea.ts
@@ -16,13 +16,15 @@ export class TextareaInput extends ValidationBase {
 		const { field, value, xform = value } = attrs;
 		const {
 			label, id, name = id, title = label, placeholder,
-			required, readonly, disabled, autofocus, autocomplete, tabindex, spellcheck,
+			required, readonly, disabled, autofocus, autocomplete,
+			minlength, maxlength, tabindex, spellcheck,
 			instant, uiClass = {}
 		} = attrs.field;
 		return m(LayoutTop, { field, value, xform, invalid: this.invalid },
 			m("textarea.w-100.bg-transparent.bn.outline-0.h-100.resize-none", {
 				id, name, title,
-				placeholder, required, readonly, disabled, autofocus, autocomplete, tabindex, spellcheck,
+				placeholder, required, readonly, disabled, autofocus, autocomplete,
+				minlength, maxlength, tabindex, spellcheck,
 				class: textareaCls(uiClass),
 				value: value(),
 				// Update value on change or input ("instant" option)
