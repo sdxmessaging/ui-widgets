51d4cec3b1b0971921b3da3937da931dae910644
Michael Pastuch <michael.pastuch@sdxmessaging.com>
2022-09-08 16:34:32 +0100

    Fix typo

--- src/interface/config.ts
+++ src/interface/config.ts
@@ -19,7 +19,7 @@ export interface IConfig {
 	showPassTxt: string;
 	/** Text to append to widget labels when field is required */
 	requiredLblPost: string;
-	/** Date local */
+	/** Date locale */
 	dateLocale: string;
 	/** Default options for signature creator */
 	signOpts: ReadonlyArray<SignTypes>;
