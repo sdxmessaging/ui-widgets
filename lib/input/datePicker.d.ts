649fee4a8f9c3203c94b3e39f8e9d10b8afa4ac3
Michael Pastuch <michael.pastuch@sdxmessaging.com>
2022-10-17 17:07:09 +0100

    Move flatpickr into own component

---
+++ lib/input/datePicker.d.ts
@@ -0,0 +1,10 @@
+import m, { ClassComponent, CVnode, CVnodeDOM } from "mithril";
+import { IPropWidget } from "../interface/widget";
+export declare function getYmd(date: Date): [string, string, string];
+export declare class DatePicker implements ClassComponent<IPropWidget> {
+    private flatpickr;
+    private valueChange;
+    oncreate({ dom, attrs: { field: { max, min }, value } }: CVnodeDOM<IPropWidget>): void;
+    onremove(): void;
+    view({ attrs: { field: { config } } }: CVnode<IPropWidget>): m.Vnode<any, any>;
+}
---
+++ src/input/datePicker.ts
@@ -0,0 +1,59 @@
+import flatpickr from "flatpickr";
+import lodash from "lodash";
+import m, { ClassComponent, CVnode, CVnodeDOM } from "mithril";
+import stream from "mithril/stream";
+import { getConfig } from "../config";
+
+import { IPropWidget } from "../interface/widget";
+
+export function getYmd(date: Date): [string, string, string] {
+	return [
+		String(date.getFullYear()),
+		lodash.padStart(String(1 + date.getMonth()), 2, "0"),
+		lodash.padStart(String(date.getDate()), 2, "0"),
+	];
+}
+
+export class DatePicker implements ClassComponent<IPropWidget> {
+
+	private flatpickr!: flatpickr.Instance;
+	private valueChange!: stream<void>;
+
+	public oncreate({ dom, attrs: { field: { max, min }, value } }: CVnodeDOM<IPropWidget>) {
+		this.flatpickr = flatpickr(dom, {
+			position: "below right",
+			disableMobile: true,
+			minDate: min,
+			maxDate: max,
+			onChange: ([newDate]) => {
+				if (newDate) {
+					value(getYmd(newDate).join("-"));
+					m.redraw();
+				}
+			}
+		});
+		// Sync value change with flatpickr
+		this.valueChange = value.map((newVal) => {
+			const date = new Date(String(newVal));
+			if (isNaN(date.valueOf())) {
+				this.flatpickr.clear();
+			} else {
+				this.flatpickr.setDate(date, false);
+			}
+		});
+	}
+
+	// Consider updating min/max attrs in onupdate event
+
+	public onremove() {
+		this.flatpickr.destroy();
+		this.valueChange.end(true);
+	}
+
+	public view({ attrs: { field: { config } } }: CVnode<IPropWidget>) {
+		return m("i.ph-2px.pv-1px", {
+			class: getConfig("datePickerIcn", config)
+		});
+	}
+
+}
