3a3df4e981fce327212375696dd5f416cbb48f80
Michael Pastuch <michael.pastuch@sdxmessaging.com>
2024-04-15 11:01:26 +0100

    full build

--- lib/currency.d.ts
+++ lib/currency.d.ts
@@ -4,6 +4,13 @@
  */
 export declare class Currency {
     private static formatter;
+    /**
+     * Format a number into a currency string
+     * @param unitTotal total in smallest monetary unit to convert e.g. 12345
+     * @param negativeParens whether to format negative numbers with parentheses
+     * @param invert interpret postive/negative `unitTotal` as the opposite
+     * @return currency string if finite number e.g. "1,234,567.89", "-1.23", "(4.56)", or undefined
+     */
     static format(unitTotal: number, negativeParens?: boolean, invert?: boolean): string | undefined;
     /**
      * Parse a currency string into a number
@@ -14,9 +21,10 @@ export declare class Currency {
     /**
      * Convert a number into a currency string
      * @param unitTotal total in smallest monetary unit to convert e.g. 12345
+     * @param format whether to format the large number with commas
      * @return currency string if finite number e.g. "123.45" or undefined
      */
-    static numtoStr(unitTotal: number): string | undefined;
+    static numtoStr(unitTotal: number, format?: boolean): string | undefined;
     /**
      * Convert a number into a currency string pair
      * @param unitTotal total in smallest monetary unit to convert e.g. 12345
