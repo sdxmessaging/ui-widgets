28e171e86fb29767660f92085b23821e02c99cca
Michael Pastuch <michael.pastuch@sdxmessaging.com>
2024-04-25 16:54:03 +0100

    full build

--- lib/list/pageController.d.ts
+++ lib/list/pageController.d.ts
@@ -1,27 +1,32 @@
 import { ListController } from "./listController";
 import { IListPageRender } from "../interface/list";
 export declare class PageController<T> extends ListController<T> {
-    /** Size of a "page" in "blocks" */
+    /** Default size of a "page" in "blocks" */
     private static readonly PAGE_STRIDE;
     static single<D>(load: () => Promise<D[]>): PageController<D>;
     /** Factory for a ListController that loads data in pages */
     static paging<D>(load: (offset: number, limit: number) => Promise<D[]>): PageController<D>;
+    private pageStride;
+    /** Update the number of "blocks" to render in a single page */
+    set blockStride(value: number);
     /** Zero-indexed page number */
     private page;
-    /** Zero-indexed last page available */
-    private get pageCount();
     /** Zero-indexed page update */
     private updatePage;
+    /** 1-indexed page number */
+    get currentPage(): number;
+    /** Number of pages to fit loaded list items */
+    get lastPage(): number;
     get canPageForward(): boolean;
     get canPageBackward(): boolean;
-    get currentPage(): number;
-    get lastPage(): number;
+    get rowsPerPage(): number;
     /** Set page */
     setPage(page: number): void;
     /** Change page relative to current page */
     pageRelative(offset: number): void;
     render<C>(callback: (params: IListPageRender<T>) => C): C[];
     debug(): {
+        blockStride: number;
         page: number;
         data: number;
         filtered: number;
