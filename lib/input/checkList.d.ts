19b70d68ae494ff3c247b363f2d5320977035c7a
Michael Pastuch <michael.pastuch@sdxmessaging.com>
2024-11-26 14:31:51 +0000

    full build

--- lib/input/checkList.d.ts
+++ lib/input/checkList.d.ts
@@ -1,11 +1,12 @@
 import m, { CVnode } from "mithril";
 import { IOptionField, IPropWidget } from "../interface/widget";
-import { IConfig, TIcon, TSubset } from "../interface/config";
 import { BaseWidget } from "../baseWidget";
+import { IConfig, TIcon, TSubset } from "../interface/config";
 type TSelectWidget = IPropWidget<IOptionField>;
 export declare class CheckList extends BaseWidget<TSelectWidget> {
     protected readonly onIcon: keyof TSubset<IConfig, TIcon>;
     protected readonly offIcon: keyof TSubset<IConfig, TIcon>;
+    private list;
     private selected;
     private open;
     private openTs;
@@ -13,16 +14,15 @@ export declare class CheckList extends BaseWidget<TSelectWidget> {
     private get focusOption();
     private set focusOption(value);
     private keySearch;
-    private keyTs;
     private toggleOpen;
     private toggleSelection;
     private moveFocus;
-    private findFocus;
+    private applyFilter;
     private keyNav;
     private placeHolder;
     /** Sync selection set with value stream */
     private syncSelection;
-    oninit({ attrs: { value } }: CVnode<TSelectWidget>): void;
+    oninit({ attrs: { field: { options }, value } }: CVnode<TSelectWidget>): void;
     onbeforeupdate({ attrs: { value } }: CVnode<TSelectWidget>): void;
     view({ attrs }: CVnode<TSelectWidget>): m.Vnode<import("../interface/widget").IPropLayoutWidget<import("../interface/widget").IField>, unknown>;
     private singleSelectionRow;
