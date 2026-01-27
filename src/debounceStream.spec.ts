4a9c88476d813c807cdbf764df6da640897f45b2
Michael Pastuch <michael.pastuch@sdxmessaging.com>
2026-01-27 09:41:33 +0000

    Update test to use debounce pipe method

--- src/debounceStream.spec.ts
+++ src/debounceStream.spec.ts
@@ -1,18 +1,19 @@
+import stream from "mithril/stream";
 import { DebounceStream } from "./debounceStream";
 
 describe("DebounceStream", () => {
 
 	it("call with last value", (done) => {
 		const debounce = new DebounceStream<number>(16);
-		debounce.stream.map((value) => {
+		const source = stream<number>();
+		debounce.pipe(source, (value) => {
 			expect(value).toBe(1);
 			debounce.destroy();
 			done();
 		});
-
-		debounce.method(3);
-		debounce.method(2);
-		debounce.method(1);
+		source(3);
+		source(2);
+		source(1);
 	});
 
 });
